bad393d0f8456f370c4ac1804ac01ec97a603b46
andrei1245 <dodea.andrei04@gmail.com>
2025-04-10 13:48:00 +0300

    added utilizator model

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const { createClient } = require('@supabase/supabase-js');
+ 
 
 // Load environment variables
 dotenv.config();
