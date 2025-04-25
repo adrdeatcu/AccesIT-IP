c6eb1de23ae391ac28739a81e613617943c9d171
andrei1245 <dodea.andrei04@gmail.com>
2025-04-25 10:14:05 +0300

    -added a real-time clock

--- Frontend/src/app/components/portar-home/portar-home.component.ts
+++ Frontend/src/app/components/portar-home/portar-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 type BarrierState = 'closed' | 'opening' | 'opened' | 'closing';
 
@@ -8,9 +8,33 @@ type BarrierState = 'closed' | 'opening' | 'opened' | 'closing';
   templateUrl: './portar-home.component.html',
   styleUrl: './portar-home.component.css'
 })
-export class PortarHomeComponent {
+export class PortarHomeComponent implements OnInit, OnDestroy {
   barrierState: BarrierState = 'closed';
   buttonDisabled = false;
+  currentTime: string = '';
+  private timeInterval: any;
+
+  ngOnInit() {
+    this.updateTime();
+    this.timeInterval = setInterval(() => {
+      this.updateTime();
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if (this.timeInterval) {
+      clearInterval(this.timeInterval);
+    }
+  }
+
+  private updateTime() {
+    const now = new Date();
+    this.currentTime = now.toLocaleTimeString('ro-RO', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    });
+  }
 
   toggleBarrier() {
     if (this.barrierState === 'closed') {
